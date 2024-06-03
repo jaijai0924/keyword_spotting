a95ae291a1f102ab0c810c5f5be9e3c4fb24e55b
Jaijai <jaijai0924@gmail.com>
2024-06-02 20:05:13 -0700

    complete initial data cleaning

---
+++ eda_helper.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+import os
+from glob import glob
+from typing import Dict, Tuple, List, 
+
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+
+
+
+
+class EDAHelper:
+    def __init__(self, file_path: str, age):
+        # self.name = name
+        self.age = age
+        self.audio_files = glob(file_path)
